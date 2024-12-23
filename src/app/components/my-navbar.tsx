154a43c8ce2b97d9291f0ca300a1f05b893b2207
Emzar Panikashvili <epanikas@gmail.com>
2024-12-24 00:19:38 +0100

    performance: added missing profile picture, added lazy attribute to the avatar image, added fixed image dimensions

--- src/app/components/my-navbar.tsx
+++ src/app/components/my-navbar.tsx
@@ -99,7 +99,7 @@ export default function MyNavbar(props: NavbarProps) {
                 <div className={"flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse"}>
                     <button type="button" className={"flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"} id={"user-menu-button"} aria-expanded={"false"} data-dropdown-toggle={"user-dropdown"} data-dropdown-placement={"bottom"}>
                         <span className={"sr-only"}>Open user menu</span>
-                        <img className={"w-8 h-8 rounded-full"} src={"/docs/images/people/profile-picture-3.jpg"} alt={"user photo"} />
+                        <img className={"w-8 h-8 rounded-full"} src={"/images/profile-picture-3.jpg"} alt={"user photo"} loading={"lazy"} width={"32px"} height={"32px"}/>
                     </button>
                     {/*Dropdown menu*/}
                     <div className={"z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"} id={"user-dropdown"}>
